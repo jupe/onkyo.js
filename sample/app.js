824ded57c2edacdef35479df368e7ee820204a70
Jussi Vatjus-Anttila <jussiva@gmail.com>
2013-10-05 09:59:53 +0300

    Several bug fixes and more commands. Added more features to sample app.

--- sample/app.js
+++ sample/app.js
@@ -1,11 +1,19 @@
 var Onkyo = require('../lib/Onkyo');
 
 var onkyo = new Onkyo();
-onkyo.Discover( function(err, device){
+onkyo.on("error", function(err){
+	console.log(err);
+});
+onkyo.on("detected", function(device){
 	console.log(device);
+});
+onkyo.on("connected", function(host){
+	console.log("connected to: "+JSON.stringify(host));
+});
+onkyo.Discover( function(err, device){
 	onkyo.Connect( function(){
-		console.log('client connected');
-	 	//onkyo.On();
-	 	onkyo.SendCommand("SOURCE_SELECT", "FM");
+	 	onkyo.PwrOff();
+	 	//onkyo.SendCommand("SOURCE_SELECT", "FM");
+	 	onkyo.Close();
 	});
 });
