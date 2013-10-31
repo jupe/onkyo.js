a6e97ee3a6f2f349f50739c0e415cbfe7911d5fe
Jussi Vatjus-Anttila <jussiva@gmail.com>
2013-10-31 19:28:03 +0200

    several smaller fixes

--- sample/app.js
+++ sample/app.js
@@ -1,20 +1,27 @@
 var Onkyo = require('../lib/Onkyo');
 
-var onkyo = new Onkyo();
+var onkyo = new Onkyo({log: true});
 onkyo.on("error", function(err){
 	console.log(err);
 });
+/*
 onkyo.on("detected", function(device){
 	console.log(device);
 });
 onkyo.on("connected", function(host){
 	console.log("connected to: "+JSON.stringify(host));
-});
+});*/
 onkyo.Discover( function(err, device){
 	onkyo.Connect( function(){
-	 	onkyo.PwrOn();
-	 	//onkyo.SendCommand("SOURCE_SELECT", "FM");
+		console.log('connected');
+	 	onkyo.PwrOn(function(error, ok){
+	 		if(error)console.log(error);
+	 		if(ok)console.log(ok);
+
+	 		
+	 	});
 	 	onkyo.VolUp();
+	 	//onkyo.SendCommand("SOURCE_SELECT", "FM");
 	 	
 	});
 });
