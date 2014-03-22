cf703e204424097e26497edf2e8a0a459b381b52
Matthew Voss <voss.matthew@gmail.com>
2014-03-21 20:58:56 -0500

    Update library

--- sample/app.js
+++ sample/app.js
@@ -1,28 +1,36 @@
-var Onkyo = require('../lib/Onkyo');
+var Onkyo = require('../lib/onkyo');
 
-var onkyo = new Onkyo({log: true});
+var onkyo = Onkyo.init({log: true});
 onkyo.on("error", function(err){
-	console.log(err);
+    console.log(err);
 });
-/*
+
 onkyo.on("detected", function(device){
-	console.log(device);
+    console.log(device);
 });
 onkyo.on("connected", function(host){
-	console.log("connected to: "+JSON.stringify(host));
-});*/
-onkyo.Discover( function(err, device){
-	onkyo.Connect( function(){
-		console.log('connected');
-	 	onkyo.PwrOn(function(error, ok){
-	 		if(error)console.log(error);
-	 		if(ok)console.log(ok);
+    console.log("connected to: "+JSON.stringify(host));
+    onkyo.PwrOn(function(error, ok){
+        if(error)console.log("error:", error);
+        if(ok)console.log("success:", ok);
+        onkyo.VolUp(function(error, ok){
+            onkyo.VolDown(function(error, ok){
+                onkyo.PwrOff(function(error, ok){
+                    onkyo.Close(function(error, ok){
+                        process.exit();
+                    });
+                });
+            });
+        });
+    });
 
-	 		
-	 	});
-	 	onkyo.VolUp();
-	 	//onkyo.SendCommand("SOURCE_SELECT", "FM");
-	 	
-	});
 });
-setTimeout( onkyo.Close, 10000);
+onkyo.Discover( function(err, device){
+    onkyo.Connect( function(){
+        console.log('connected');
+
+        //onkyo.SendCommand("SOURCE_SELECT", "FM");
+
+    });
+});
+//setTimeout( onkyo.Close, 10000);
